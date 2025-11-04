52de22df496a72e13b8d8c381161254f0a0ecf16
Angel moncayo <amoncayo42@tntech.edu>
2025-11-04 13:06:17 -0600

    transaction + catgeory progress

--- App/category.py
+++ App/category.py
@@ -6,11 +6,37 @@ getters and setters
 """
 
 # TODO: Create a Category class with the attributes name and budget_limit. All attributes should be declared as private. 
+class Category:
 
+    def __init__(self, name, budget_limit):
+        self.__name = name
+        self.__budget_limit = budget_limit
     # TODO: Write getters and setters for all attributes (2 getters and 2 setters)
+
+    def get_name(self):
+        return self.__name
+
+    def get_budget_limit(self):
+        return self.__budget_limit
+
+    def set_name(self, name):
+        self.__name = name
+
+    def set_budget_limit(self, budget_limit):
+        self.__budget_limit = budget_limit
+
+
 
     """
     This is a helper function for finding the category summaries
     """
     # TODO: Create a get_spent_amount method that uses a list of transactions to find the total spent from transactions in a certain category.
-    
+
+
+    def get_spent_amount(self, all_transactions):
+        total_spent = 0
+        for transaction in all_transactions:
+            if transaction.get_category_name() == self.__name:
+                total_spent = total_spent + transaction.get_amount()
+
+        return total_spent
--- App/transaction.py
+++ App/transaction.py
@@ -7,4 +7,35 @@ getters and setters
 # TODO: Create a Transaction class with the attributes amount, category_name, description, and date. All attributes should be
 #       declared as private.
 
+class Transaction:
+    def __init__(self, amount, category_name, description, date):
+        self.__amount = amount
+        self.__category_name = category_name
+        self.__description = description
+        self.__date = date
+
     # TODO: Write getters and setters for all attributes (4 getters and 4 setters)
+
+    def get_Amount(self):
+        return self.__amount
+
+    def get_category_name(self):
+        return self.__category_name
+
+    def get_description(self):
+        return self.__description
+
+    def get_date(self):
+        return self.__date
+
+    def set_amount(self, amount):
+        self.__amount = amount
+
+    def set_category_name(self, category_name):
+        self.__category_name = category_name
+
+    def set_description(self, description):
+        self.__description = description
+
+    def set_date(self, date):
+        self.__date = date
